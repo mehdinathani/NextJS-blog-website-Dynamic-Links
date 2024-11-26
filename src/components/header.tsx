c7337ec2dc7d26b407612e3abdf8e4e6e1bc8c22
mehdinathani <mehdinathani@gmail.com>
2024-11-26 08:27:40 +0500

    init

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="bg-blue-900 text-white py-6">
             <div className="max-w-6xl mx-auto flex items-center justify-between px-4 md:px-8">
-                <h1 className="text-2xl font-bold">Mehdi's Blog</h1>
+                <h1 className="text-2xl font-bold">Mehdi&rsquo; Blog</h1>
                 <nav>
                     <ul className="flex gap-4 md:gap-8">
                         <li>
